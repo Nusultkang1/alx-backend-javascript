e3c334f9fcafdf0c3b7199fe1b6287a42a2aa3ec
nurusul <nurudeensultan28@gmail.com>
2023-06-29 23:18:06 +0100

    Task 3

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,8 +1,8 @@
-function getFullResponseFromAPI(success)
-	return new Promise((resolve, reject) => {
-		if (success) resolve({ status: 200, body: 'Success' });
-		else reject(Error('The fake API is not working currently'));
-	});
+function getResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if (success) resolve({ status: 200, body: 'Success' });
+    else reject(new Error('The fake API is not working currently'));
+  });
 }
 
-export default getFullResponseFromAPI;
+export default getResponseFromAPI;
